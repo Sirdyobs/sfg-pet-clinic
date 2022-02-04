bc3c9d009f5d01cbb6c9d88c39b09f473dd5092e
luxolo <dyobiso.luxolo@gmail.com>
2022-02-05 00:13:06 +0200

    Implemented interface for services. Closes #5.

---
+++ pet-clinic-data/src/main/java/guru/framework/sfgpetclinic/services/PetService.java
@@ -0,0 +1,11 @@
+package guru.framework.sfgpetclinic.services;
+
+import guru.framework.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+    Pet findById(Long id);
+    Pet save(Pet owner);
+    Set<Pet> findAll();
+}
